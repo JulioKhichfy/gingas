74553967f34b0649e27fb479a206128d6b1f8d2b
julio cesar khichfy <julio.khichfy@mj.gov.br>
2022-11-24 12:43:15 -0300

    melhorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { BodyVideosComponent } from './body-videos/body-videos.component';
-
-import * as $ from 'jquery';
-import { BodySejaComponent } from './body-seja/body-seja.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { DoacaoComponent } from './doacao/doacao.component';
-import { AulaCapoeiraComponent } from './aula-capoeira/aula-capoeira.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SendEmailService } from './send-email.service';
@@ -22,12 +16,9 @@ import { DindowndownComponent } from './dindowndown/dindowndown.component';
   declarations: [
     AppComponent,
     NavbarComponent,
-    BodyVideosComponent,
-    BodySejaComponent,
     HomeComponent,
     FooterComponent,
     DoacaoComponent,
-    AulaCapoeiraComponent,
     InstitutoComponent,
     DindowndownComponent
   ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,14 +2,27 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl: './home.component.html', 
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  
+  public title:string='';
+  public src:string='';
 
   ngOnInit(): void {
+  }
+
+  fecharModal(){
+    $('#myiframe').removeAttr("src");
+  }
+
+  addSrc(target:string, src:string){
+    this.title = target;
+    this.src = src;
+    $('#myiframe').attr("src",src);
   }
 
 }
